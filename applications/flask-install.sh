a8bba17339bd3a37b23476b75efaf234810bfe46
Roni Dover <roni.dover@gmail.com>
2018-02-08 10:44:24 +0200

    updated

---
+++ applications/flask-install.sh
@@ -0,0 +1,17 @@
+flask-install.sh
+#install git
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+
+#install python
+# sudo apt-get install build-essential checkinstall
+# sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+# version=2.7.13
+# cd ~/Downloads/
+# wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+# tar -xvf Python-$version.tgz
+# cd Python-$version
+# ./configure
+# make
+# sudo checkinstall
