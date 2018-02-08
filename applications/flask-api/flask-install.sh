4defc9f529fe0b57414a9640ab3d693f8e0cf5e3
Roni Dover <roni.dover@gmail.com>
2018-02-08 16:54:45 +0200

    update

--- applications/flask-api/flask-install.sh
+++ applications/flask-api/flask-install.sh
@@ -3,7 +3,9 @@ flask-install.sh
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install git
-
+git clone https://github.com/doppleware/example_flask_rest_app
+pip install -r tasks/requirements.txt
+invoke app.run
 #install python
 # sudo apt-get install build-essential checkinstall
 # sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
