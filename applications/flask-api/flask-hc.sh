29db40d18ef6eded554a63d6f41d7c08021ee871
Roni Dover <roni.dover@gmail.com>
2018-02-08 23:28:27 +0200

    updated hc

--- applications/flask-api/flask-hc.sh
+++ applications/flask-api/flask-hc.sh
@@ -1,15 +1,2 @@
-exit 0
-#install git
-
-
-#install python
-# sudo apt-get install build-essential checkinstall
-# sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-# version=2.7.13
-# cd ~/Downloads/
-# wget https://www.python.org/ftp/python/$version/Python-$version.tgz
-# tar -xvf Python-$version.tgz
-# cd Python-$version
-# ./configure
-# make
-# sudo checkinstall
+bash -c 'while [[ "$(curl -s -o /dev/null -w \
+''%{http_code}'' $1:5000/api/v1/)" != "200" ]]; do sleep 5; done'
