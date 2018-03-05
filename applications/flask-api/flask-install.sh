0c0ddfc078edcb9e573819fe11e5e4ae89909727
doppleware <roni.dover@gmail.com>
2018-03-05 16:02:59 +0200

    updated syntx

--- applications/flask-api/flask-install.sh
+++ applications/flask-api/flask-install.sh
@@ -10,7 +10,6 @@ echo installing requirements
 yes | pip install -r tasks/requirements.txt
 echo running app
 cp local_config.py.template local_config.py
-invoke app.run
 #install python
 # sudo apt-get install build-essential checkinstall
 # sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
